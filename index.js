20bcb0c3f30ed64089e926a3ad26cc343fbe5926
HaarikaR <hramadugu@gmail.com>
2021-09-21 16:00:20 -0700

    Done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+   return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activity = "go to the office"){
+     
+      return `This Monday, I will ${activity}.`;
+
+}
+
+function wrapAdjective(star = "*"){
+    return function(adjective = " a hard worker"){
+        return `You are ${star}${adjective}${star}!`;
+    };
+}
+wrapAdjective("You are")("a dedicated programmer");
